fd9940762d2b6f24e48064fd58f56402883644f3
Vinicius Zuppa <vcordeiro12@gmail.com>
2021-07-22 21:10:25 -0300

    Personalizando o retorno da validação

--- src/main/java/com/vinicius/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/vinicius/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -4,9 +4,10 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
 import com.vinicius.cursomc.services.exceptions.DataIntegrityException;
 import com.vinicius.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -25,5 +26,14 @@ public class ResourceExceptionHandler {
 	}
 	
 	
+	@ExceptionHandler(org.springframework.web.bind.MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request){
+		ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Erro de validação", System.currentTimeMillis());
+		//Percorre lista de erros que tem na exceção, para cada erro gera um objeto fielMessage.
+		for(FieldError x : e.getBindingResult().getFieldErrors()) {
+			err.addError(x.getField(), x.getDefaultMessage());
+		}
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
 }
 	
